e0f1f35c6138149c8c78187c92b5efe634a152ac
Beniamin Ardelean <beniaminardelean@yahoo.com>
2021-10-28 19:09:32 +0300

    fibbonaci method

--- src/drills/Algorithms.java
+++ src/drills/Algorithms.java
@@ -1,36 +1,49 @@
 package drills;
 
 public class Algorithms {
-	
+
 	public int bigDiff(int[] nums) {
 		int minim = nums[0];
 		int maxim = nums[0];
-		for(int i = 0; i < nums.length; i ++) {
-			if(nums[i] < minim) {
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] < minim) {
 				minim = nums[i];
 			}
-			if(nums[i] > maxim) {
+			if (nums[i] > maxim) {
 				maxim = nums[i];
 			}
 		}
 		return maxim - minim;
 	}
-	
+
 	public boolean firstLast6(int[] nums) {
-		if(nums[0] == 6) {
+		if (nums[0] == 6) {
 			return true;
-		}else if (nums[nums.length-1] == 6) {
+		} else if (nums[nums.length - 1] == 6) {
 			return true;
 		}
-		 return false; 
+		return false;
 	}
-	
+
 	public String doubleChar(String str) {
 		String result = "";
-		for(int i = 0; i < str.length(); i ++) {
+		for (int i = 0; i < str.length(); i++) {
 			result = result + str.charAt(i) + str.charAt(i);
 		}
 		return result;
+	}
+
+	public static int fib(int n) {
+		int f[] = new int[n + 2];
+		int i;
+
+		f[0] = 0;
+		f[1] = 1;
+
+		for (i = 2; i <= n; i++) {
+			f[i] = f[i - 1] + f[i - 2];
+		}
+		return f[n];
 	}
 
 }
