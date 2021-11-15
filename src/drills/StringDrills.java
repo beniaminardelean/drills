273fefe6b5dab282b675bbd0dcb5e5917231ad2f
Beniamin Ardelean <beniaminardelean@yahoo.com>
2021-11-15 10:11:58 +0200

    Reverse a String

--- src/drills/StringDrills.java
+++ src/drills/StringDrills.java
@@ -2,4 +2,14 @@ package drills;
 
 public class StringDrills {
 
+	public static String reverse(String num) {
+		if (num == null || num.isEmpty()) {
+			return num;
+		}
+		String reverse = " ";
+		for (int i = num.length() - 1; i >= 0; i--) {
+			reverse = reverse + num.charAt(i);
+		}
+		return reverse;
+	}
 }
