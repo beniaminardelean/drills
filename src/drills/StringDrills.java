53da4a4cc1ded8e5cb9922d8f70ed9cc2542c89e
Beniamin Ardelean <beniaminardelean@yahoo.com>
2021-11-22 08:19:02 +0200

    count duplicate characters

--- src/drills/StringDrills.java
+++ src/drills/StringDrills.java
@@ -1,5 +1,8 @@
 package drills;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class StringDrills {
 
 	public static String reverse(String num) {
@@ -11,5 +14,28 @@ public class StringDrills {
 			reverse = reverse + num.charAt(i);
 		}
 		return reverse;
+	}
+	
+	public static void countDuplicateCharacters(String str) {
+
+		Map<Character, Integer> map = new HashMap<Character, Integer>();
+
+		char[] charArray = str.toCharArray();
+
+		for (char c : charArray) {
+
+			if (map.containsKey(c)) {
+				map.put(c, map.get(c) + 1);
+			} else {
+				map.put(c, 1);
+			}
+		}
+
+		for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+
+			if (entry.getValue() > 1) {
+				System.out.println(entry.getKey() + " : " + entry.getValue());
+			}
+		}
 	}
 }
