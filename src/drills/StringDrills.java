c7077c4acbb2cdd6367b5cade8e2f0f70d69a5ca
Beniamin Ardelean <beniaminardelean@yahoo.com>
2022-01-03 21:37:11 +0200

    String Palindrome

--- src/drills/StringDrills.java
+++ src/drills/StringDrills.java
@@ -122,4 +122,21 @@ public class StringDrills {
 					System.out.println("Strings are not anagram");
 				}
 				}
+				{
+				String str = "ab cd", reverseStr = "";
+			    
+			    int strLength = str.length();
+
+			    for (int i = (strLength - 1); i >=0; --i) {
+			      reverseStr = reverseStr + str.charAt(i);
+			      System.out.println(reverseStr);
+			    }
+
+			    if (str.toLowerCase().equals(reverseStr.toLowerCase())) {
+			      System.out.println(str + " is a Palindrome String.");
+			    }
+			    else {
+			      System.out.println(str + " is not a Palindrome String.");
+			    }
+}
 }
